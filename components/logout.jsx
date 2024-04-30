c1c2f8c2fa65212caf4724b58d913ebd82046aa9
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2024-04-30 18:02:47 +0600

    update 1

--- components/logout.jsx
+++ components/logout.jsx
@@ -1,13 +1,14 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { Domain } from "@/utils/constants";
 
 export default function Logout() {
   const router = useRouter();
 
   const handleLogout = async () => {
     try {
-      await fetch("/api/logout", {
+      await fetch(`${Domain}/api/logout`, {
         method: "GET",
       });
       router.push("/login");
