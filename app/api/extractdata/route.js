e28e876f0b1d3956db56e16bf6f6e2d2fca6d373
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2024-05-01 15:00:35 +0600

    update 7

--- app/api/extractdata/route.js
+++ app/api/extractdata/route.js
@@ -5,18 +5,18 @@ import connect from "@/lib/mongodb";
 import User from "@/lib/models/User";
 
 export async function GET(request) {
-  await connect();
-  try {
-    const userID = await getDataFromToken(request);
-    const user = await User.findById({ _id: userID }).select("-password");
-    return NextResponse.json(
-      { message: "User found", data: user },
-      {
-        status: 200,
-        success: true,
-      }
-    );
-  } catch {
-    return NextResponse.json({ error: error.message }, { status: 400 });
-  }
+  // await connect();
+  // try {
+  //   const userID = await getDataFromToken(request);
+  //   const user = await User.findById({ _id: userID }).select("-password");
+  //   return NextResponse.json(
+  //     { message: "User found", data: user },
+  //     {
+  //       status: 200,
+  //       success: true,
+  //     }
+  //   );
+  // } catch {
+  //   return NextResponse.json({ error: error.message }, { status: 400 });
+  // }
 }
