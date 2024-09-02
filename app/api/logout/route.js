2b04cc721c9e335a4e85848fd1eddd2d8be4096c
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2024-09-02 12:25:12 +0600

    update 2

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -7,7 +7,10 @@ export async function GET() {
       success: true,
     });
 
-    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+    response.cookies.set("token", "", {
+      httpOnly: false, //here I have changed httpOnly: false, it was true
+      expires: new Date(0),
+    });
 
     return response;
   } catch (error) {
